05f06f9eade8e90aea23c497bb43e53557a48362
AsyaKovach <asyakovach1996@gmail.com>
2022-12-28 13:36:29 +0300

    Specialization

---
+++ Specialization/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arg1 = new string[5] {"567", "789", "hello", "world", ":)"};
+string[] arg2 = new string[arg1.Length];
+void ArrayFormation(string[] arg1, string[] arg2)
+{
+    int count = 0;
+    for (int i = 0; i < arg1.Length; i++)
+    {
+    if(arg1[i].Length <= 3)
+        {
+        arg2[count] = arg1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write($"{arg[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayFormation(arg1, arg2);
+PrintArray(arg2);
